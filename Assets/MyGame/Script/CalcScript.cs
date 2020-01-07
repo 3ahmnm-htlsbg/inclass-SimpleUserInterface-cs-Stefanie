d0cf858c32102b34ae6496a59bd31c6bbf471b85
unknown <stefanie.zwingenberger03@htl-salzburg.ac.at>
2020-01-07 14:48:33 +0100

    add Script

---
+++ Assets/MyGame/Script/CalcScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CalcScript : MonoBehaviour
+{
+    public Text NameOut;
+    public Text CountOut;
+    public Button Namebtn;
+    public Button Countbtn;
+    public InputField Namein;
+    public int CountNumber = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetName()
+    {
+        NameOut.text = Namein.text;
+    }
+
+    public void CountUp()
+    {
+        CountNumber++;
+        CountOut.text = CountNumber.ToString();
+    }
+}
